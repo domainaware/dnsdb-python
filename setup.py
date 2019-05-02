09865a2c924ebcec518bd4eaafefc41111c17ddc
Sean Whalen <whalenster@gmail.com>
2019-05-02 10:45:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=["gcpdns"],
+    py_modules=["dnsdb"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
