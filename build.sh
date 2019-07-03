37b65979fe78f07dd090b976f15aaec520eef948
Sean Whalen <whalenster@gmail.com>
2019-07-02 23:04:20 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-pip3 install -U -r requirements.txt && rstcheck README.rst && cd docs && make html && cp -r _build/html/* ../../dnsdb-python-docs/  && flake8 gcpdns.py && rm -rf dist/ build/ && python3 setup.py sdist && python3 setup.py bdist_wheel
+pip3 install -U -r requirements.txt && rstcheck README.rst && cd docs && make html && cp -r _build/html/* ../../dnsdb-python-docs/ && cd ..  && flake8 dnsdb.py && rm -rf dist/ build/ && python3 setup.py sdist && python3 setup.py bdist_wheel
 
